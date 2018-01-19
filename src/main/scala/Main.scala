c0a721c38aac8dbe9326404753114283e85eaad4
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-20 00:07:18 +0100

    add es behind proxy to DataFrame

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,14 +7,30 @@ object Main {
   def main(args: Array[String]) {
     val spark = SparkSessionHelper.buildSession()
     // write to elasticsearch local (docker)
-    val df2 = spark.read.format("json").load("data/people.json")
-    df2.show()
-    df2.saveToEs("people/person")
+    //    val df2 = spark.read.format("json").load("data/people.json")
+    //    df2.show()
+    //    df2.saveToEs("people/person")
+
+    val options =Map(
+      "es.read.field.include" -> "",
+      "es.index.auto.create" -> "true",
+      "pushdown" -> "true",
+      "es.nodes"-> "",
+      "es.port" -> "443",
+      "es.net.http.auth.user" -> "",
+      "es.net.http.auth.pass" -> "",
+      "es.net.ssl" -> "true",
+      "es.nodes.discovery" -> "false",
+      "es.nodes.resolve.hostname" -> "false",
+      "es.nodes.wan.only" -> "true"
+    )
 
     // read from elasticsearch local
     val df = spark.read
       .format("es")
-      .load("people/person")
+      .options(options)
+      .load("")
+
     df.show()
     spark.close()
   }
