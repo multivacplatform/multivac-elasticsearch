6b7a63d45b073e32f54ccdc930a65560dead3526
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-04-26 14:33:37 +0200

    Update elasticsearch.scala

--- src/test/scala/elasticsearch.scala
+++ src/test/scala/elasticsearch.scala
@@ -2,6 +2,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class elasticsearch extends AnyFlatSpec {
   "elasticsearch" should "correctly download and transform the data" in {
-    Politic.main(Array.empty)
+    RetweetGraph.main(Array.empty)
   }
 }
